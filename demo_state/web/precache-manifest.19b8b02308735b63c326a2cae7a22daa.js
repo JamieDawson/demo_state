45588ff43b5abe3b4a950a430968f6e3b1023aef
JamieDawson <jamierobertdawson@gmail.com>
2020-07-05 13:59:03 -0700

    I got it working. But I need to work on the steps

---
+++ demo_state/web/precache-manifest.19b8b02308735b63c326a2cae7a22daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2c0f44c8c58ed69154617dca9e85d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b292f766c339472a7dad",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "051ac6cf16cfc7bc1918",
+    "url": "/static/js/2.2c0df106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c0df106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b292f766c339472a7dad",
+    "url": "/static/js/main.191cbf3d.chunk.js"
+  },
+  {
+    "revision": "bf82cc81f5abaf170727",
+    "url": "/static/js/runtime-main.8a298d7d.js"
+  }
+]);
